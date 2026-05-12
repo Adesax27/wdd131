527544ec5cf73be683086a0912c369f5728780d5
Adesax27 <ademolaokunjoyo@gmail.com>
2026-05-12 14:58:41 +0100

    album

---
+++ scripts/temples.js
@@ -0,0 +1,12 @@
+// Dynamic Year and Last Modified
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = `Last Modified: ${document.lastModified}`;
+
+// Hamburger Menu Toggle
+const mainnav = document.querySelector('.navigation');
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('show');
+    hambutton.classList.toggle('open');
+});
